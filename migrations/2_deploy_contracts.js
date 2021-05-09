cb708a48dde830c0cac03c8d3ad5418536cb95ef
benmacdonald <ben@perpetualabs.com>
2021-05-09 14:54:07 -0400

    add bakeryswap router address

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -4,6 +4,6 @@ module.exports = function (deployer) {
   deployer.deploy(
     Arbitrage,
     "0xBCfCcbde45cE874adCB698cC183deBcF17952812", //PancakeSwap factory
-    "TODO" //BakerySwap router
+    "0xcde540d7eafe93ac5fe6233bee57e1270d3e330f" //BakerySwap router
   );
 };
